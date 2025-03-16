9d32a4c2a51bc4e9c49c5a8284c8e4a6a14cd729
mukesh22584 <mks22584@gmail.com>
2025-03-16 15:13:51 +0530

    Initial gms checkout - Taken from: https://gitlab.com/MindTheGapps/vendor_gapps

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+# Merge split apps
+cat vendor/gms/common/proprietary/product/priv-app/Velvet/Velvet_part_* > vendor/gms/common/proprietary/product/priv-app/Velvet/Velvet.apk
+cat vendor/gms/common/proprietary/product/priv-app/GmsCore/GmsCore_part_* > vendor/gms/common/proprietary/product/priv-app/GmsCore/GmsCore.apk
